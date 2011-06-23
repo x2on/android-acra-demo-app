432896d51cc96c67abe4877877052022299e2592
Felix Schulze <code@felixschulze.de>
2011-06-23 17:12:49 +0200

    Button for crashing app

--- src/main/java/de/felixschulze/androidacrademoapp/HelloAndroid.java
+++ src/main/java/de/felixschulze/androidacrademoapp/HelloAndroid.java
@@ -2,22 +2,22 @@ package de.felixschulze.androidacrademoapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
 
-
-public class HelloAndroid extends Activity {
-
-	/**
-	 * Called when the activity is first created.
-	 * 
-	 * @param savedInstanceState
-	 *            If the activity is being re-initialized after previously being
-	 *            shut down then this Bundle contains the data it most recently
-	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
-	 *            is null.</b>
-	 */
-	@Override
+public class HelloAndroid extends Activity implements OnClickListener {
+    @Override
     public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-	}
+        Button button = (Button) findViewById(R.id.Button01);
+        button.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        System.out.println("Crash");
+        Object nul = null;
+        nul.toString();
+    }
 }
